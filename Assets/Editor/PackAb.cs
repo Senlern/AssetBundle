4d3883b59eb78ce4b51dbe2543aa3f825932b3fc
zhangxinlin <429350061@qq.com>
2021-10-09 19:05:38 +0800

    开发提交

---
+++ Assets/Editor/PackAb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class PackAb : MonoBehaviour {
+    [MenuItem("buildAB/windows", false, 100)]
+    public static void BuildWindowsResource()
+    {
+        TextEditor te = new TextEditor();
+        string streamPath = Application.streamingAssetsPath;
+        if (Directory.Exists(streamPath))
+        {
+            Directory.Delete(streamPath, true);
+        }
+        Directory.CreateDirectory(streamPath);
+        BuildPipeline.BuildAssetBundles(streamPath, BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+}
